341ef17c1db56b5bb7b4dab08fd68a1cea26327b
Oleksandr Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-10-03 00:37:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 [Be Concise IV - Index of an element in an array](https://www.codewars.com/kata/be-concise-iv-index-of-an-element-in-an-array)
-
+```
 function find(array, element) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === element) return i;
   }
   return "Not found";
 }
+```
