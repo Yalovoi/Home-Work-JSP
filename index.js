a0b9090f41dbf53ebf0a9ba4d616c0d2787c8f6c
Oleksandr Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-10-03 15:41:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function excludingVatPrice(price){
   return price === null? -1:+((price/1.15).toFixed(2));
 }
 
-let excludingVatPrice = price => price === null? -1:+((price/1.15).toFixed(2));
+let excludingVatPrice = price => price === null? -1:+((price/1.15).toFixed(2))
+
 ```
                                                   
